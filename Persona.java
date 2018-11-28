6699ed8a93b84cbaace50b296b76e298ee4edcdd
Luciano Albornoz <luciano.albornoz@mercadolibre.com>
2018-11-28 12:23:12 -0300

    agrega nombre
    agrega apellido

--- Persona.java
+++ Persona.java
@@ -1,6 +1,8 @@
 public class Persona {
   
     private int edad;
+    private String nombre;
+    private String apellido;
 
     public void mensaje() {
         System.out.println("Soy una persona");
